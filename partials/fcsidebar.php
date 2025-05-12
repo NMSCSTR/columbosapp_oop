ddf9e1920bfd3e298eaa98b5fab26a0476a431ff
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-05-12 14:10:03 +0800

    done with logout

--- partials/fcsidebar.php
+++ partials/fcsidebar.php
@@ -57,6 +57,20 @@
                             class="text-gray-700 transition-opacity duration-300">Profile</span>
                     </a>
                 </li> -->
+                <li>
+                    <a href="#" id="logoutBtn"
+                        class="flex items-center space-x-3 px-4 py-2 hover:bg-gray-100 rounded-lg transition-colors"
+                        :class="{ 'bg-gray-100': activeSection === 'profile' }">
+                        <svg class="shrink-0 w-5 h-5 text-red-600 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
+                    </svg>
+                        <span
+                            :class="{ 'block opacity-100': openSidebar || window.innerWidth >= 768, 'hidden opacity-0': !openSidebar && window.innerWidth < 768 }"
+                            class="text-gray-700 transition-opacity duration-300">Logout</span>
+                    </a>
+                </li>
             </ul>
         </nav>
     </aside>
