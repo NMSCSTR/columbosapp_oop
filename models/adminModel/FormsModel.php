564a66569c21894aa28cdafb41b84427d96a9665
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-05-23 04:10:25 +0800

    making model for forms and fraternal counselor

---
+++ models/adminModel/FormsModel.php
@@ -0,0 +1,29 @@
+<?php 
+class FormsModel{
+    private $conn;
+
+    public function __construct($dbConnection)
+    {
+        $this->conn = $dbConnection;
+    }
+
+    public function viewAllForms(){
+
+    }
+
+    public function addForms(){
+
+    }
+
+    public function downloadForms(){
+
+    }
+
+    public function deleteForms(){
+
+    }
+
+    
+}
+
+?>
---
+++ models/fraternalCounselorModel/FraternalCounselorModel.php
@@ -0,0 +1,28 @@
+<?php 
+class FraternalCounselorModel{
+    private $conn;
+
+    public function __construct($dbConnection)
+    {
+        $this->conn = $dbConnection;
+    }
+
+    public function viewAllRoaster(){
+
+    }
+
+    public function viewAllocationById($id){
+
+    }
+
+    public function viewForms(){
+
+    }
+
+    public function downloadForms(){
+
+    }
+
+    
+}
+?>
